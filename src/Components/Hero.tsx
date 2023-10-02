c03ca02655f288259b825d265c889e1bbfa75884
Emmanuel Olaniyi <olaimarnoel@gmail.com>
2023-10-02 19:13:32 +0100

    working on projects

---
+++ src/Components/Hero.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+type Props = {};
+
+const Hero = (props: Props) => {
+  return (
+    <div>
+      <div className="flex items-center">
+        <p className="ss">Samurai King Resting</p>
+        <button className="bg-[#000] h-[47px] w-[257px]">ADD TO CART</button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "../assets/logo.png";
+import cart from "../assets/cart.svg";
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return (
+    <div>
+      <div className="w-full lg:mb-10 mb-4 flex justify-between items-center px-4 xl:px-20 ">
+        <img src={logo} alt="" className="w-[8rem] object-contain" />
+        <img src={cart} alt="" className="w-[2.4rem] object-contain" />
+      </div>
+
+      <div className=" xl:px-20">
+        <div className="h-[4px] bg-[#E4E4E4] "></div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
