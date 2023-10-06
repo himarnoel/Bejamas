d63c723d23ca6616d47683cbae2e6bc142f95e3c
Emmanuel Olaniyi <olaimarnoel@gmail.com>
2023-10-06 13:08:03 +0100

    removed bug

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -8,11 +8,12 @@ const Navbar = () => {
     <div>
       <div className="w-full lg:mb-10 mb-4 flex justify-between items-center px-4 xl:px-20 ">
         <img src={logo} alt="" className="w-[8rem] object-contain" />
-        <img src={cart} alt="" className="w-[2.4rem] object-contain" />
+        <img src={cart} alt="" className="w-[2.4rem] object-contain" onClick={()=>setfirst(true)}/>
       </div>
       <div className=" xl:px-20">
         <div className="h-[4px] bg-[#E4E4E4] "></div>
         <div
+
           className={
             first
               ? "float-right h-[10rem] right-20 absolute w-[26rem] z-20 bg-red-200"
